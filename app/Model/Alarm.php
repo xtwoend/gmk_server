80506f6c9a0ef5f839564b158ef9554d2e8f1f40
Abdul Hafidz Anshari <aditans88@gmail.com>
2023-05-11 14:31:15 +0700

    update

--- app/Model/Alarm.php
+++ app/Model/Alarm.php
@@ -6,6 +6,7 @@ use Carbon\Carbon;
 use Hyperf\Database\Schema\Schema;
 use Hyperf\DbConnection\Model\Model;
 use Hyperf\Database\Schema\Blueprint;
+use Hyperf\Database\Model\Events\Created;
 use Hyperf\Database\Model\Events\Creating;
 
 class Alarm extends Model
@@ -39,6 +40,38 @@ class Alarm extends Model
         $this->id = \Ramsey\Uuid\Uuid::uuid4()->toString();
     }
 
+    public function created(Created $event)
+    {
+        $model = $event->getModel();
+
+        $device = $model->device;
+        $topics = [
+            1 => 'data/gmk/k/leepack1/alarm',
+            2 => 'data/gmk/k/leepack2/alarm',
+            3 => 'data/gmk/k/leepack3/alarm',
+            7 => 'data/gmk/k/lme1/alarm',
+            8 => 'data/gmk/k/lme2/alarm',
+            9 => 'data/gmk/k/lme3/alarm'
+        ];
+
+        if($device) {
+            $topic = $topics[$device->id] ?? 'data/gmk/k/general';
+            $listen = 'mqtt_1';
+            $config = config('mqtt.servers')[$listen];
+            $clientId = \Hyperf\Utils\Str::random(10);
+            $event = $this->event;
+            $mqtt = new \PhpMqtt\Client\MqttClient($config['host'], $config['port'], $clientId);
+            
+            $config = (new \PhpMqtt\Client\ConnectionSettings)
+                ->setUsername($config['username'])
+                ->setPassword($config['password']);
+
+            $mqtt->connect($config, true);
+            $mqtt->publish($topic, json_encode($model->toArray()), 0);
+            $mqtt->disconnect();
+        }
+    }
+
     /**
      * get duration attribute
      */
@@ -48,10 +81,17 @@ class Alarm extends Model
         if($duration > 0) {
             return gmdate("H:i:s", $duration);
         }
-
         return 0;
     }
-    
+
+    /**
+     * relation to device
+     */
+    public function device()
+    {
+        return $this->belongsTo(Device::class, 'device_id');
+    }
+
     /**
      * create or choice table
      */
