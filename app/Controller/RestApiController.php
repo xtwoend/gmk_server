6f7e1f1182d36b83761dba1615e5b3ad5ffb788e
Abdul Hafidz Anshari <aditans88@gmail.com>
2023-05-11 18:38:19 +0700

    update

---
+++ app/Controller/RestApiController.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\MdModel\Startup;
+use App\MdModel\Production;
+use App\MdModel\ProductionRecord;
+use App\MdModel\ProductionProblem;
+use App\MdModel\StartupVerification;
+use App\MdModel\ProductionVerification;
+use Hyperf\HttpServer\Contract\RequestInterface;
+use Hyperf\HttpServer\Contract\ResponseInterface;
+
+class RestApiController
+{
+    public function index($table, RequestInterface $request)
+    {
+        if($table == 'startup') {
+            $model = new Startup;
+        }elseif($table == 'startup_verifications') {
+            $model = new StartupVerification;
+        }elseif($table == 'productions') {
+            $model = new Production;
+        }elseif($table == 'production_verifications') {
+            $model = new ProductionVerification;
+        }elseif($table == 'production_records') {
+            $model = new ProductionRecord;
+        }elseif($table == 'production_problems') {
+            $model = new ProductionProblem;
+        }
+
+        
+    }
+}
