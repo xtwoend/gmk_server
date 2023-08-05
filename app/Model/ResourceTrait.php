7b06d1ef11c1fc6cc238edf8038c238d12425119
Abdul Hafidz Anshari <aditans88@gmail.com>
2023-08-05 10:48:10 +0700

    update

--- app/Model/ResourceTrait.php
+++ app/Model/ResourceTrait.php
@@ -9,8 +9,11 @@ trait ResourceTrait
     {
         $data = collect($data)->map(function($val, $key){
             return collect($val)->map(function($val, $key){
+                $castType = $this->getCastType($key);
                 if($this->hasCast($key, ['array', 'json']) && in_array(trim(strtolower($this->getCasts()[$key])),['array', 'json'])) {
                     return $this->fromJson($val);
+                } elseif($castType == 'decimal'){
+                    return $this->asDecimal($value, explode(':', $this->getCasts()[$key], 2)[1]);
                 }else{
                     return $val;
                 }
