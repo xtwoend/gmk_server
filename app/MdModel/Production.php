30b922eb120ca5591277ad722f9454befa000df2
Abdul Hafidz Anshari <aditans88@gmail.com>
2023-09-14 10:21:11 +0700

    update

--- app/MdModel/Production.php
+++ app/MdModel/Production.php
@@ -36,7 +36,7 @@ class Production extends Model
 
     public function product()
     {
-        return $this->belongsTo(Product::class, 'product_id');
+        return $this->belongsTo(Product::class, 'product_id', 'code');
     }
 
     public function productionVerifications()
