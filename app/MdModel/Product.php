0b0d1ebb8f9fe357429272143dd50661e4ec7e38
Abdul Hafidz Anshari <aditans88@gmail.com>
2023-09-14 10:15:00 +0700

    update

--- app/MdModel/Product.php
+++ app/MdModel/Product.php
@@ -17,11 +17,16 @@ class Product extends Model
      * The attributes that are mass assignable.
      */
     protected array $fillable = [
-        'code', 'name', 'unit', 'desc'
+        'code', 'product_name', 'unit', 'desc'
     ];
 
     /**
      * The attributes that should be cast to native types.
      */
     protected array $casts = [];
+
+
+    public function getNameAttribute(){
+        return $this->product_name;
+    }
 }
